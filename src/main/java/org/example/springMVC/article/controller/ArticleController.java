546a6a1928b9020fbb5fa7a8b5969e06525a5572
gangpro <kangmh86@korea.ac.kr>
2020-03-06 18:11:55 +0900

    프로젝트 구조 변경

--- src/main/java/org/example/springMVC/article/controller/ArticleController.java
+++ src/main/java/org/example/springMVC/article/controller/ArticleController.java
@@ -3,12 +3,10 @@ package org.example.springMVC.article.controller;
 import org.example.springMVC.article.service.ArticleService;
 import org.example.springMVC.article.vo.ArticleVO;
 import org.example.springMVC.commons.paging.Criteria;
-import org.example.springMVC.commons.paging.PageMaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,11 +14,13 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.inject.Inject;
 
+// 프로젝트 구조 변경
+// 기본적인 CRUD
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
 
-    private static final Logger logger = LoggerFactory.getLogger(ArticleController.class);
+    private static final Logger logger = LoggerFactory.getLogger(OriginalArticleController.class);
 
     private final ArticleService articleService;
 
@@ -35,7 +35,7 @@ public class ArticleController {
     public String writeGET() {
         logger.info("write GET...");
 
-        return "/article/write";
+        return "article/normal/write";
     }
 
     // 등록
@@ -48,7 +48,7 @@ public class ArticleController {
         articleService.create(articleVO);
         redirectAttributes.addFlashAttribute("msg", "regSuccess");
 
-        return "redirect:/article/list";
+        return "redirect:/article/normal/list";
     }
 
     // 목록
@@ -58,7 +58,15 @@ public class ArticleController {
         logger.info("list...");
         model.addAttribute("article", articleService.listAll());
 
-        return "/article/list";
+        return "article/normal/list";
+    }
+
+    // 목ㄹ고
+    @RequestMapping(value = "/listCriteria", method = RequestMethod.GET)
+    public String listCriteria(Model model, Criteria criteria) throws Exception {
+        logger.info("normal listCriteria() ...");
+        model.addAttribute("articles", articleService.listCriteria(criteria));
+        return "article/normal/list_criteria";
     }
 
     // 조회
@@ -68,7 +76,7 @@ public class ArticleController {
         logger.info("read...");
         model.addAttribute("article", articleService.read(articleNo));
 
-        return "/article/read";
+        return "article/normal/read";
     }
 
     // 수정
@@ -79,7 +87,7 @@ public class ArticleController {
         logger.info("normal modifyGet() called ...");
         model.addAttribute("article", articleService.read(articleNo));
 
-        return "article/modify";
+        return "article/normal/modify";
     }
 
     // 수정
@@ -101,84 +109,6 @@ public class ArticleController {
         redirectAttributes.addFlashAttribute("msg", "delSuccess");
 
         return "redirect:/article/list";
-    }
-
-    // 페이징 처리
-    @RequestMapping(value = "/listCriteria", method = RequestMethod.GET)
-    public String listCriteria(Model model, Criteria criteria) throws Exception {
-        logger.info("listCriteria... ");
-        model.addAttribute("articles", articleService.listCriteria(criteria));
-
-        return "/article/list_criteria";
-    }
-
-    // 페이지 번호 출력처리가 된 목록 페이지를 처리할 메서드
-    @RequestMapping(value = "/listPaging", method = RequestMethod.GET)
-    public String listPaging(Model model, Criteria criteria) throws Exception {
-        // Criteria, Model 타입의 변수 criteria 와 model 을 파라미터로 사용
-        // Model 객체를 사용하여 PageMaker 에서 계산한 결과 값을 저장
-        logger.info("listPaging...");
-
-        PageMaker pageMaker = new PageMaker();
-        pageMaker.setCriteria(criteria);
-//        pageMaker.setTotalCount(1000);  // 아직 영속계층에서 전체 게시글의 갯수를 구하는 로직을 구현하지 않았기 때문에 setTotalCount 의 매개변수는 1000을 임의로 넣었다.
-        pageMaker.setTotalCount(articleService.countArticles(criteria));
-
-        model.addAttribute("articles", articleService.listCriteria(criteria));
-        model.addAttribute("pageMaker", pageMaker);
-
-        return "/article/list_paging";
-    }
-
-    // 조회(개선: 목록 페이지 정보 유지)
-    @RequestMapping(value = "/readPaging", method = RequestMethod.GET)
-    public String readPaging(@RequestParam("articleNo") int articleNo,
-                             @ModelAttribute("criteria") Criteria criteria,
-                             Model model) throws Exception {
-        logger.info("readPaging...");
-        model.addAttribute("article", articleService.read(articleNo));
-
-        return "/article/read_paging";
-    }
-
-    // 수정 페이지 이동(개선: 목록 페이지 정보 유지)
-    @RequestMapping(value = "/modifyPaging", method = RequestMethod.GET)
-    public String modifyGETPaging(@RequestParam("articleNo") int articleNo,
-                                  @ModelAttribute("criteria") Criteria criteria,
-                                  Model model) throws Exception {
-
-        logger.info("normal modifyGetPaging...");
-        model.addAttribute("article", articleService.read(articleNo));
-
-        return "article/modify_paging";
-    }
-
-    // 수정 페이지 처리(개선: 목록 페이지 정보 유지)
-    @RequestMapping(value = "/modifyPaging", method = RequestMethod.POST)
-    public String modifyPOSTPaging(ArticleVO articleVO,
-                                   Criteria criteria,
-                                   RedirectAttributes redirectAttributes) throws Exception {
-        logger.info("modifyPOSTPaging...");
-        articleService.update(articleVO);
-        redirectAttributes.addFlashAttribute("page", criteria.getPage());
-        redirectAttributes.addFlashAttribute("perPageNum",criteria.getPerPageNum());
-        redirectAttributes.addFlashAttribute("msg", "modSuccess");
-
-        return "redirect:/article/listPaging";
-    }
-
-    // 삭제(개선: 목록 페이지 정보 유지)
-    @RequestMapping(value = "/removePaging", method = RequestMethod.POST)
-    public String removePaging(@RequestParam("articleNo") int articleNo,
-                               Criteria criteria,
-                               RedirectAttributes redirectAttributes) throws Exception {
-        logger.info("remove...");
-        articleService.delete(articleNo);
-        redirectAttributes.addFlashAttribute("page", criteria.getPage());
-        redirectAttributes.addFlashAttribute("perPageNum", criteria.getPerPageNum());
-        redirectAttributes.addFlashAttribute("msg", "delSuccess");
-
-        return "redirect:/article/listPaging";
     }
 
 }
---
+++ src/main/java/org/example/springMVC/article/controller/ArticlePagingSearchController.java
@@ -0,0 +1,135 @@
+package org.example.springMVC.article.controller;
+
+import org.example.springMVC.article.service.ArticleService;
+import org.example.springMVC.article.vo.ArticleVO;
+import org.example.springMVC.commons.paging.Criteria;
+import org.example.springMVC.commons.paging.PageMaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.inject.Inject;
+
+// 프로젝트 구조 변경
+// 기본적인 CRUD + 페이징 처리 + 검색 처리 구현
+@Controller
+@RequestMapping("/article/paging/search")
+public class ArticlePagingSearchController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ArticlePagingSearchController.class);
+
+    private final ArticleService articleService;
+
+    @Inject
+    public ArticlePagingSearchController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+//
+//    // 등록
+//    // 등록 페이지 이동
+//    @RequestMapping(value = "/write", method = RequestMethod.GET)
+//    public String writeGET() {
+//        logger.info("paging writeGET()...");
+//
+//        return "/article/paging/write";
+//    }
+//
+//    // 등록
+//    // 등록 처리
+//    @RequestMapping(value = "/write", method = RequestMethod.POST)
+//    public String writePOST(ArticleVO articleVO,
+//                            RedirectAttributes redirectAttributes) throws Exception {
+//        logger.info("paging writePOST()...");
+//        logger.info(articleVO.toString());
+//
+//        articleService.create(articleVO);
+//        redirectAttributes.addFlashAttribute("msg", "regSuccess");
+//
+//        return "redirect:/article/paging/list";
+//    }
+//
+//    // 목록
+//    // 목록 페이지 이동
+//    // 페이징 처리
+//    // 페이지 번호 출력처리가 된 목록 페이지를 처리할 메서드
+//    @RequestMapping(value = "/list", method = RequestMethod.GET)
+//    public String list(Model model, Criteria criteria) throws Exception {
+//        logger.info("paging list()...");
+//
+//        PageMaker pageMaker = new PageMaker();
+//        pageMaker.setCriteria(criteria);
+//        pageMaker.setTotalCount(articleService.countArticles(criteria));
+//
+//        model.addAttribute("article", articleService.listCriteria(criteria));
+//        model.addAttribute("pageMaker", pageMaker);
+//
+//        return "/article/paging/list";
+//    }
+//
+//    // 조회
+//    // 조회 페이지 이동
+//    // 조회(개선: 목록 페이지 정보 유지)
+//    @RequestMapping(value = "/read", method = RequestMethod.GET)
+//    public String read(@RequestParam("articleNo") int articleNo,
+//                       @ModelAttribute("criteria") Criteria criteria,
+//                       Model model) throws Exception {
+//        logger.info("paging read()...");
+//        model.addAttribute("article", articleService.read(articleNo));
+//
+//        return "/article/paging/read";
+//    }
+//
+//    // 수정
+//    // 수정 페이지 이동
+//    // 수정 페이지 이동(개선: 목록 페이지 정보 유지)
+//    @RequestMapping(value = "/modify", method = RequestMethod.GET)
+//    public String modifyGET(@RequestParam("articleNo") int articleNo,
+//                            @ModelAttribute("criteria") Criteria criteria,
+//                            Model model) throws Exception {
+//
+//        logger.info("paging modifyGet()...");
+//        model.addAttribute("article", articleService.read(articleNo));
+//
+//        return "article/paging/modify";
+//    }
+//
+//    // 수정
+//    // 수정 처리
+//    // 수정 페이지 처리(개선: 목록 페이지 정보 유지)
+//    @RequestMapping(value = "/modify", method = RequestMethod.POST)
+//    public String modifyPOST(ArticleVO articleVO,
+//                             Criteria criteria,
+//                             RedirectAttributes redirectAttributes) throws Exception {
+//        logger.info("paging modifyPOST()...");
+//
+//        articleService.update(articleVO);
+//        redirectAttributes.addAttribute("page", criteria.getPage());
+//        redirectAttributes.addAttribute("perPageNum", criteria.getPerPageNum());
+//        redirectAttributes.addFlashAttribute("msg", "modSuccess");
+//
+//        return "redirect:/article/paging/list";
+//    }
+//
+//    // 삭제
+//    // 삭제(개선: 목록 페이지 정보 유지)
+//    @RequestMapping(value = "/remove", method = RequestMethod.POST)
+//    public String remove(@RequestParam("articleNo") int articleNo,
+//                         Criteria criteria,
+//                         RedirectAttributes redirectAttributes) throws Exception {
+//        logger.info("paging remove()...");
+//
+//        articleService.delete(articleNo);
+//        redirectAttributes.addAttribute("page", criteria.getPage());
+//        redirectAttributes.addAttribute("perPageNum", criteria.getPerPageNum());
+//        redirectAttributes.addFlashAttribute("msg", "delSuccess");
+//
+//        return "redirect:/article/paging/list";
+//    }
+
+}
